b950fadb73cfa21d3cd3a0dee3cc18cddcfea337
KIRAN G N <143183067+Kirangn24@users.noreply.github.com>
2025-03-25 15:52:59 +0530

    Update EmployeeInterface.java

--- src/main/java/com/ps/BankingApp/EmployeeInterface.java
+++ src/main/java/com/ps/BankingApp/EmployeeInterface.java
@@ -10,7 +10,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse; 
 @WebServlet("/login")
 public class EmployeeInterface extends HttpServlet
 {
